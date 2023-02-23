8c9854cebc537e32d1f54e80ccac4a9a3f8bcbfa
Zaho92 <alex.altrogge@gmail.com>
2023-02-23 22:58:01 +0100

    DisplayChanges

---
+++ WorkTimer/Converter/DecimalToTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WorkTimer.Converter
+{
+    public class DecimalToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal hours)
+            {
+                TimeSpan timeSpan = TimeSpan.FromHours((double)hours);
+                return string.Format("{0:D2}:{1:mm}", (int)timeSpan.TotalHours, timeSpan);
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
